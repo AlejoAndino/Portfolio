37e59d22abe54f749f1576d6faed3189413e92ae
AleAndino <aleandino.cba@gmail.com>
2023-05-13 15:39:22 -0300

    Home

---
+++ src/components/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Layout = ({children, className=""}) => {
+  return (
+    <div className={`${className}`}>
+      {children}
+    </div>
+    
+  )
+}
+
+export default Layout
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,18 +2,23 @@ import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
 import logo from '../assets/logo.png'
+import { useRouter } from 'next/router'
 
-const NavBar = () => {
+const NavBar = () => { 
+
+  const router = useRouter();
+  console.log(router);
+
   return (
     <header className='w-full h-[120px] px-10 py-10 font-medium flex justify-between items-center bg-dark'>
 
       <Image src={logo} height={'100'} width={'100'} alt='logo' />
 
       <nav className='text-light pr-10'>
-        <Link className='mr-6' href='/'>Home</Link>
-        <Link className='mr-6' href='/about'>About</Link>
-        <Link className='mr-6' href='/projects'>Projects</Link>
-        <Link className='mr-6' href='/contact'>Contact</Link>
+        <Link className='mr-6 relative group' href='/'>Home <span className={`h-[2px] inline-block bg-light absolute left-0 -bottom-1 group-hover:w-full transition-[width] ease duration-300 ${router.asPath === '/' ? "w-full" : 'w-0'}`}>&nbsp;</span></Link>
+        <Link className='mr-6 relative group' href='/about'>About <span className={`h-[2px] inline-block bg-light absolute left-0 -bottom-1 group-hover:w-full transition-[width] ease duration-300 ${router.asPath === '/about' ? "w-full" : 'w-0'}`}>&nbsp;</span></Link>
+        <Link className='mr-6 relative group' href='/projects'>Projects <span className={`h-[2px] inline-block bg-light absolute left-0 -bottom-1 group-hover:w-full transition-[width] ease duration-300 ${router.asPath === '/projects' ? "w-full" : 'w-0'}`}>&nbsp;</span></Link>
+        <Link className='mr-6 relative group' href='/contact'>Contact <span className={`h-[2px] inline-block bg-light absolute left-0 -bottom-1 group-hover:w-full transition-[width] ease duration-300 ${router.asPath === '/contact' ? "w-full" : 'w-0'}`}>&nbsp;</span></Link>
       </nav>
     </header>
   )
